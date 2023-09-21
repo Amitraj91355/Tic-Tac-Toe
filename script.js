073732a111858f18050b5f2f38abd9f7b5c36bcd
Amit Raj <117329500+Amitraj91355@users.noreply.github.com>
2023-09-21 16:54:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
-console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
-let turn = "X"
+// Welcome message
+console.log("Welcome to Tic Tac Toe");
+
+// Audio elements for game sounds
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+
+// Initial player turn
+let turn = "X";
+
+// Flag to track game over state
 let isgameover = false;
 
 // Function to change the turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
-} 
-
-// Function to check for a win
-const checkWin = ()=>{
-    let boxtext = document.getElementsByClassName('boxtext');
-    let wins = [
-        [0, 1, 2, 5, 5, 0], 
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
-        }
-    })
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X";
 }
 
+// Function to check for a win
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let win of wins) {
+        if (
+            boxtext[win[0]].innerText === boxtext[win[1]].innerText &&
+            boxtext[win[1]].innerText === boxtext[win[2]].innerText &&
+            boxtext[win[0]].innerText !== ""
+        ) {
+            // Effect after a delay
+            setTimeout(() => {
+                gameover.play();
+
+                // Change the background color of the winning cells
+                for (let cell of win) {
+                    document.getElementsByClassName("box")[cell].style.backgroundColor =
+                        "rgb(112, 255, 112)";
+                }
+
+                // Display the winner in the info div
+                document.querySelector(".info").innerText = boxtext[win[0]].innerText + " Won";
+                document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            }, 250);
+
+            // Set the game over flag
+            isgameover = true;
+        }
+    }
+};
+
 // Game Logic
-// music.play()
 let boxes = document.getElementsByClassName("box");
 
 Array.from(boxes).forEach(element => {
@@ -54,14 +77,20 @@ Array.from(boxes).forEach(element => {
 });
 
 // Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
+reset.addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
+        element.innerText = "";
     });
-    turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
+    turn = "X";
+    isgameover = false;
+
+    // Reset the background color of all cells to white
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.backgroundColor = "white";
+    });
+
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+});
